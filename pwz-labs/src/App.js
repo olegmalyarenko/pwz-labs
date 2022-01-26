d266128b61bdf320aed97df8106c731002aaff9a
unknown <oi.malyarenko@gmail.com>
2022-01-26 10:52:10 +0200

    change title

--- pwz-labs/src/App.js
+++ pwz-labs/src/App.js
@@ -9,7 +9,7 @@ function App() {
       <div className="app-title">
         <h2>ЗВІТИ З ЛАБОРАТОРНИХ РОБІТ</h2>
         <h2>З ДИСЦИПЛІНИ "РОЗРОБКА WEB-ЗАСТОСУВАНЬ"</h2>
-        <div className="row"><span>БРИГАДА №17</span> <span style={{paddingLeft:30}}>СТУДЕНТИ: Маляренко О.І.,  Дмитренко М.В.,  Дмитренко Р.А.</span></div>
+        <div className="row"><span>БРИГАДА №7</span> <span style={{paddingLeft:30}}>СТУДЕНТИ: Маляренко О.І.,  Дмитренко М.В.,  Дмитренко Р.А.</span></div>
       </div>
      < TabList/>
     </div>
